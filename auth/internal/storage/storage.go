2ba77f2ee9a60ea6c5559e676e10b3edaeff0cb2
imyazip <tohchac@gmail.com>
2024-11-21 21:52:21 +0300

    Изменена структура бд, добавлены функции для работы с БД

--- auth/internal/storage/storage.go
+++ auth/internal/storage/storage.go
@@ -24,3 +24,37 @@ func (s *Storage) FindAPIKeyInStorage(ctx context.Context, apiKey string) (bool,
 	}
 	return exists, nil
 }
+
+// RoleExists проверяет существование роли и возвращает ее ID
+func (s *Storage) RoleExists(ctx context.Context, roleName string) (int, error) {
+	var roleID int
+	err := s.db.QueryRowContext(ctx, "SELECT id FROM roles WHERE role_name = $1", roleName).Scan(&roleID)
+	if err != nil {
+		return -1, err
+	}
+
+	return roleID, nil
+}
+
+// UserExists проверяет существование пользователя
+// Возвращает false в случае отсутствия пользователя с указанным username в БД
+func (s *Storage) UserExists(ctx context.Context, userName string) (bool, error) {
+	var exists bool
+	err := s.db.QueryRowContext(ctx, "SELECT COUNT(*) FROM users WHERE username = ?", userName).Scan(&exists)
+	if err != nil {
+		return true, err
+	}
+
+	return false, nil
+}
+
+// InsertUser добавляет нового пользователя в БД,
+// Возвращает err в случае неудачи, nil при успешном добавлении
+func (s *Storage) InsertUser(ctx context.Context, userName string, hashedPassword string, roleID int) error {
+	_, err := s.db.ExecContext(ctx, "INSERT INTO users (username, password, role_id) VALUES (?, ?, ?)", userName, hashedPassword, roleID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- auth/migrations/init.sql
+++ auth/migrations/init.sql
@@ -1,8 +1,25 @@
-CREATE DATABASE IF NOT EXISTS auth_db;
-
-CREATE TABLE IF NOT EXISTS auth_db.api_keys (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    key_value VARCHAR(255) NOT NULL UNIQUE
+-- Создание таблиц
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    role_name VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT
 );
 
-INSERT INTO auth_db.api_keys (key_value) VALUES ('your-api-key');
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+CREATE TABLE api_keys (
+    id SERIAL PRIMARY KEY,
+    api_key VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    revoked BOOLEAN DEFAULT FALSE
+);
+
+
