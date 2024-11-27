d686ca3ed39a5d6eef8ce758c4bdf6c53a979087
imyazip <tohchac@gmail.com>
2024-11-28 01:37:27 +0300

    Реализована бизнес логика сервиса

--- log_storage/internal/service/service.go
+++ log_storage/internal/service/service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/imyazip/GoSIEM/log-storage/internal/storage"
@@ -30,4 +32,36 @@ func NewLogService(db *storage.Storage, config config.Config) *LogService {
 		storage:    db,
 		authClient: authClient,
 	}
+}
+
+func (s *LogService) ValidateJWT(ctx context.Context) (bool, error) {
+	jwt, ok := ctx.Value("jwt").(string)
+	if !ok || jwt == "" {
+		return false, fmt.Errorf("missing JWT token")
+	}
+
+	resp, err := s.authClient.ValidateJWTKey(ctx, &pb.ValidateJWTKeyRequest{JWT: jwt})
+	if err != nil {
+		return false, fmt.Errorf("failed to validate JWT: %w", err)
+	}
+
+	if !resp.Valid {
+		return false, fmt.Errorf("invalid JWT token")
+	}
+
+	return true, nil
+}
+
+// SaveRawLog сохраняет сырой строковый лог в хранилище
+func (s *LogService) SaveRawLog(ctx context.Context, req *pb.TransferRawStringLogRequest, sensorID int64) error {
+	return s.storage.InsertRawLog(req.LogSource, req.LogString, req.SystemCreatedAt.AsTime(), req.SensorId)
+}
+
+// SaveSerializedLog сохраняет сериализованные логи в хранилище
+func (s *LogService) SaveSerializedLog(ctx context.Context, req *pb.TranserSerializedLogRequest, sensorID int64) error {
+	serializedData, err := json.Marshal(req.LogSerialized)
+	if err != nil {
+		return fmt.Errorf("failed to serialize log data: %w", err)
+	}
+	return s.storage.InsertSerializedLog(req.LogSource, string(serializedData), req.SystemCreatedAt.AsTime(), fmt.Sprintf("%d", sensorID))
 }
