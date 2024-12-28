001d104a0bacdb6b01f2c876805d35539659adda
imyazip <tohchac@gmail.com>
2024-12-28 05:17:41 +0300

    added network analyzer

--- network_analyze/go.mod
+++ network_analyze/go.mod
@@ -1,3 +1,22 @@
 module github.com/imyazip/network_analyze
 
 go 1.23.2
+
+require (
+	github.com/google/gopacket v1.1.19
+	github.com/imyazip/sigolyze v0.1.0
+	google.golang.org/grpc v1.69.2
+	google.golang.org/protobuf v1.35.1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/cloudflare/ahocorasick v0.0.0-20240916140611-054963ec9396 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+)
--- network_analyze/main.go
+++ network_analyze/main.go
@@ -1,1 +1,74 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/imyazip/network_analyze/config"
+	"github.com/imyazip/sigolyze"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/imyazip/network_analyze/proto"
+)
+
+func main() {
+	cfg := config.LoadConfig("analyzer.yaml")
+	conn, err := grpc.NewClient(fmt.Sprintf("%s:%d", cfg.LogServer.Host, cfg.LogServer.Port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to gRPC server: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewLogStorageServiceClient(conn)
+
+	compiler := sigolyze.NewCompiler()
+	compiler.LoadSignatureFromJson("./signatures/sig.json")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <interface>")
+		os.Exit(1)
+	}
+
+	// Получение интерфейса из аргументов командной строки
+	interfaceName := os.Args[1]
+
+	// Открытие устройства для прослушивания
+	handle, err := pcap.OpenLive(interfaceName, 65536, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatalf("Error opening device %s: %v", interfaceName, err)
+	}
+	defer handle.Close()
+
+	// Установка фильтра для IP-пакетов (опционально)
+	err = handle.SetBPFFilter("ip")
+	if err != nil {
+		log.Fatalf("Error setting BPF filter: %v", err)
+	}
+
+	fmt.Printf("Listening on interface %s for IP packets...\n", interfaceName)
+
+	// Создание канала пакетов
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		matches := sigolyze.Match(compiler, packet.String())
+		if len(matches) != 0 {
+			log.Printf("Matched with signature: %s", matches[0].Name)
+			req := &pb.AddSecurityEventRequest{
+				LogId:            0,
+				EventType:        matches[0].Name,
+				EventDescription: "",
+			}
+
+			_, err := pb.LogStorageServiceClient.AddSecurityEvent(client, context.Background(), req)
+			if err != nil {
+				log.Printf("Error sending event: %s", err)
+			}
+		}
+	}
+
+}
